243f8b76a6979a64551820ac41458645d1cc527a
ieshan sharma <ieshansharma456@gmail.com>
2021-08-05 23:00:01 +0530

    My First

--- app.js
+++ app.js
@@ -64,11 +64,13 @@ app.post("/Failure",function (req,resp) {
   resp.redirect("/");
 })
 
-};
 
-app.listen(process.env.PORT || 3000){
-  console.log("Server is running on port 3000");
+
+app.listen( process.env.PORT || 3000, function(){
+    console.log("Server is running on port 3000");
 });
+
+
 
 
 
